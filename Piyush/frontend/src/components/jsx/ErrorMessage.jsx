07af2c7afbe200131de30f4aa4879c092143a2ad
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-06-25 18:25:27 +0530

    Revert "Added a ChatPage, setup authentication and backend for registration a…"
    This reverts commit 2de26c37eb386bd8be87e1a252e07e5b35178fb3.

--- Piyush/frontend/src/components/jsx/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-import styles from '../css/ErrorMessage.module.css';
-
-function ErrorMessage({ message }) {
-  return message ? <p className={styles.text}>{message}</p> : null;
-}
-
-export default ErrorMessage;
